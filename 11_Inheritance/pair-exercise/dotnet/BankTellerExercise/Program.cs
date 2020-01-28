78171d0c26babe19d3ec18acec1d5b732b415cb8
Christina Rivera <cnrivera@gmail.com>
2020-01-27 21:46:39 -0500

    Updated comments on completed pair exercise M1 W3 Inheritance 1

--- 11_Inheritance/pair-exercise/dotnet/BankTellerExercise/Program.cs
+++ 11_Inheritance/pair-exercise/dotnet/BankTellerExercise/Program.cs
@@ -8,6 +8,8 @@ namespace BankTellerExercise
         static void Main(string[] args)
         {
 
+            // Testing part one math in the various account rules. Result should be -60, 150.
+
             BankAccount checkingAccount = new CheckingAccount();
             BankAccount savingsAccount = new SavingsAccount();
 
@@ -24,19 +26,20 @@ namespace BankTellerExercise
             checkingAccount.Transfer(savingsAccount, amountToTransfer);
 
 
+            //Testing part two results.
 
             Console.WriteLine(checkingAccount.Balance);
             Console.WriteLine(savingsAccount.Balance);
 
-            // checkingAccount = new CheckingAccount();
-            // savingsAccount = new SavingsAccount();
+            checkingAccount = new CheckingAccount();
+            savingsAccount = new SavingsAccount();
 
-            //BankCustomer jayGatsby = new BankCustomer();
-            //jayGatsby.AddAccount(checkingAccount);
-            //jayGatsby.AddAccount(savingsAccount);
+            BankCustomer jayGatsby = new BankCustomer();
+            jayGatsby.AddAccount(checkingAccount);
+            jayGatsby.AddAccount(savingsAccount);
 
 
-            //Console.WriteLine($"Jay Gatsby has {jayGatsby.Accounts.Length} accounts.");
+            Console.WriteLine($"Jay Gatsby has {jayGatsby.Accounts.Length} accounts.");
         }
     }
 }
